c94be0cdff601132dc16d600af09efd6deb1cb18
Happysword <45149700+Happysword@users.noreply.github.com>
2022-10-30 02:41:11 +0200

    News like other pages

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,69 +1,88 @@
 <template>
-  <div class="news-container">
-    <Loading v-if="waitRequest"></Loading>
-
-    <!--News Header-->
-    <v-container v-else class="new-container">
-      <v-row justify="center" align="center" class="mt-10">
-        <v-col cols="12" class="center-horizontal"
-          ><h2 class="text-h3 text-center font-weight-light">
-            {{ language.news }}
-          </h2></v-col
-        >
-      </v-row>
-      <!-- Stories -->
-      <v-row
-        v-if="stories.length === 0"
-        class="mb-10 mt-5"
-        justify="center"
-        align="center"
-      >
-        <v-col cols="auto">
-          <h2 class="text-overline">
-            {{ language.noNews }}
-          </h2>
-        </v-col>
-      </v-row>
-      <v-row class="mb-10 mt-5" v-else id="infinite-list">
-        <v-col
-          v-for="(story, i) in stories"
-          :key="i"
-          :class="{
-            'col-6': $vuetify.breakpoint.mdAndUp,
-            'col-12': $vuetify.breakpoint.smAndDown
-          }"
-        >
-          <v-card color="white" elevation="2" class="pa-0">
-            <router-link append :to="String(story.id)" class="remove-underline">
-              <v-img :src="ImageSrc(story.id)" height="300"></v-img>
-              <v-row
-                justify="center"
-                align="center"
-                class="mb-5 px-10 pt-5 pb-5"
-                no-gutters
-              >
-                <v-col cols="12" class="text-h5">
-                  {{ story.title }}
-                </v-col>
-              </v-row>
-            </router-link>
-            <v-row justify="center">
-              <v-col
-                cols="auto"
-                v-if="
-                  $store.state.currentUser &&
-                    $store.state.currentUser.type === 'admin'
-                "
-              >
-                <v-btn x-small color="error" @click="deleteNews(story.id)">
-                  {{ language.delete }}
-                </v-btn></v-col
-              >
-            </v-row>
-          </v-card>
-        </v-col>
-      </v-row>
+  <div>
+    <!-- Hero Container -->
+    <v-container fluid class="hero-container white--text">
+      <v-container class="new-container">
+        <v-row dense align="center">
+          <v-col cols="12" class="text-center">
+            <h1
+              class="font-weight-bold mb-5"
+              :class="{
+                'text-h4': $vuetify.breakpoint.xs,
+                'text-h3': $vuetify.breakpoint.sm,
+                'text-h1': $vuetify.breakpoint.mdAndUp
+              }"
+            >
+              {{ language.news }}
+            </h1>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-container>
+
+    <div class="news-container">
+      <Loading v-if="waitRequest"></Loading>
+
+      <!--News Header-->
+      <v-container v-else class="new-container">
+        <!-- Stories -->
+        <v-row
+          v-if="stories.length === 0"
+          class="mb-10 mt-5"
+          justify="center"
+          align="center"
+        >
+          <v-col cols="auto">
+            <h2 class="text-overline">
+              {{ language.noNews }}
+            </h2>
+          </v-col>
+        </v-row>
+        <v-row class="mb-10 mt-5" v-else id="infinite-list">
+          <v-col
+            v-for="(story, i) in stories"
+            :key="i"
+            :class="{
+              'col-6': $vuetify.breakpoint.mdAndUp,
+              'col-12': $vuetify.breakpoint.smAndDown
+            }"
+          >
+            <v-card color="white" elevation="2" class="pa-0">
+              <router-link
+                append
+                :to="String(story.id)"
+                class="remove-underline"
+              >
+                <v-img :src="ImageSrc(story.id)" height="300"></v-img>
+                <v-row
+                  justify="center"
+                  align="center"
+                  class="mb-5 px-10 pt-5 pb-5"
+                  no-gutters
+                >
+                  <v-col cols="12" class="text-h5">
+                    {{ story.title }}
+                  </v-col>
+                </v-row>
+              </router-link>
+              <v-row justify="center">
+                <v-col
+                  cols="auto"
+                  v-if="
+                    $store.state.currentUser &&
+                      $store.state.currentUser.type === 'admin'
+                  "
+                >
+                  <v-btn x-small color="error" @click="deleteNews(story.id)">
+                    {{ language.delete }}
+                  </v-btn></v-col
+                >
+              </v-row>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
 
     <!--footer-->
     <Footer></Footer>
@@ -135,6 +154,18 @@ export default {
 </script>
 
 <style scoped>
+.hero-container {
+  background: url(../assets/news1.jpg) center center/cover no-repeat;
+  width: 100%;
+  box-shadow: inset 0 0 0 1000px rgba(0, 0, 0, 0.2);
+  object-fit: contain;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 80px);
+}
+
 .news-container {
   background-color: #f5f5f5;
 }
@@ -146,9 +177,6 @@ export default {
 .remove-underline {
   text-decoration: none;
   color: black;
-}
-.new-container {
-  min-height: calc(100vh - 80px);
 }
 @media (min-width: 1904px) {
   .new-container {
